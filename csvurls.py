a9ed13f110e3a77deed4b82ef70dcef2bbbd1d76
suggy33 <saggar.co@gmail.com>
2020-05-08 16:55:02 +1000

    initial

---
+++ csvurls.py
@@ -0,0 +1,34 @@
+import requests
+import csv
+import time
+import webbrowser
+import keyboard
+
+
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+index = 0
+https: // github.com/suggy33/PythonLoopOverCsvUrls.git
+
+urls = []
+# r = requests.get(row)
+with open('test.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        urls.append(row)
+
+print(urls)
+# make sure all urls have http:// or https://
+
+
+for url in urls:
+    for link in url:
+        r = requests.get(link)
+        r.encoding = 'utf-8'
+        print(r)
+        index += 1
+        print(index)
+        # webbrowser.get(chrome_path).open(link)
+
+    html_content = r.text
+    print('fetched')
